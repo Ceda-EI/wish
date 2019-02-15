3440f16b567c246920a684088f71a6330418786b
Ceda EI <ceda_ei@protonmail.com>
2019-02-15 19:57:48 +0530

    Wrap init in a function

--- wish.sh
+++ wish.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# Source all plugins
-for i in ${WISH_PLUGINS[@]}; do
-	for j in "$XDG_CONFIG_HOME" "/usr/share" "$HOME/.config"; do
-		source "$j/wish/plugins/$i.sh" &> /dev/null && break
+function wish_init() {
+	# Source all plugins
+	for i in ${WISH_PLUGINS[@]}; do
+		for j in "$XDG_CONFIG_HOME" "/usr/share" "$HOME/.config"; do
+			source "$j/wish/plugins/$i.sh" &> /dev/null && break
+		done
+		[[ $? -ne 0 ]] && echo "Plugin $i not found." >&2
 	done
-	[[ $? -ne 0 ]] && echo "Plugin $i not found." >&2
-done
 
-# Source theme
-WISH_THEME=${WISH_THEME:-plain}
-for i in "$XDG_CONFIG_HOME" "/usr/share" "$HOME/.config"; do
-	source "$i/wish/themes/$WISH_THEME.sh" &> /dev/null && break
-done
+	# Source theme
+	WISH_THEME=${WISH_THEME:-plain}
+	for i in "$XDG_CONFIG_HOME" "/usr/share" "$HOME/.config"; do
+		source "$i/wish/themes/$WISH_THEME.sh" &> /dev/null && break
+	done
 
-[[ $? -ne 0 ]] && echo "Theme $WISH_THEME not found." >&2
+	[[ $? -ne 0 ]] && echo "Theme $WISH_THEME not found." >&2
+}
+
+wish_init
