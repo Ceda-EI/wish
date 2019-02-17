25e5c14130614bc471827bd202c53759f5c0930f
Ceda EI <ceda_ei@protonmail.com>
2019-02-17 11:10:13 +0530

    Set colors for every plugin in plain theme.

--- themes/plain.sh
+++ themes/plain.sh
@@ -1,3 +1,7 @@
 WISH_DEFAULT_BG=-1
 WISH_DEFAULT_FG=-1
 WISH_POWERLINE=0
+for i in ${WISH_PLUGINS[@]}; do
+	eval WISH_$(echo $i | tr '[:lower:]' '[:upper:]')_BG=-1
+	eval WISH_$(echo $i | tr '[:lower:]' '[:upper:]')_FG=-1
+done
