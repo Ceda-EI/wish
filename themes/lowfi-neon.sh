d9e86e4421339bcf77456d5b1f2f26692ba015fe
Ceda EI <ceda_ei@webionite.com>
2019-07-18 17:16:19 +0530

    Add theme lowfi-neon

---
+++ themes/lowfi-neon.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+WISH_POWERLINE=0
+local i
+local gradient=(e7c547 c0e551 82e35a 62e177 6bdfb3 73d4dd 7aa6da)
+local j=0
+for i in ${WISH_PLUGINS[@]}; do
+	[[ $i == "newline" ]] && j=0 && continue
+	eval WISH_$(echo $i | tr '[:lower:]' '[:upper:]')_BG=-1
+	eval WISH_$(echo $i | tr '[:lower:]' '[:upper:]')_FG=${gradient[$j]}
+	((j++))
+	if [[ $j -eq ${#gradient[@]} ]]; then
+		j=0
+	fi
+done
