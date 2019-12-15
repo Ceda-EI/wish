eb7f3a6aa39bb02987f96892b43ff40ba5fe4446
Ceda EI <ceda_ei@webionite.com>
2019-12-15 22:00:37 +0530

    Update rgb_gradient to support right prompt.

--- themes/rgb_gradient.sh
+++ themes/rgb_gradient.sh
@@ -13,3 +13,17 @@ for i in ${WISH_PLUGINS[@]}; do
 		j=0
 	fi
 done
+
+j=0
+for i in ${WISH_RIGHT_PLUGINS[@]}; do
+	[[ $i == "newline" ]] && j=0 && continue
+	eval WISH_${i^^}_BG=${gradient[$((${#WISH_RIGHT_PLUGINS[@]} - $j - 1))]}
+	x=WISH_${i^^}_BG
+	echo WISH_${i^^}_BG: ${!x} ${#gradient[@]} - ${#WISH_RIGHT_PLUGINS[@]} - $j
+	eval WISH_${i^^}_FG=${fg_gradient[$((${#WISH_RIGHT_PLUGINS[@]} - $j - 1))]}
+	echo WISH_${i^^}_FG: $WISH_${i^^}_FG
+	((j++))
+	if [[ $j -eq ${#gradient[@]} ]]; then
+		j=0
+	fi
+done
