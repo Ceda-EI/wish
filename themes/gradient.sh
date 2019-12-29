343e394d407d1e0bfe1ca14e8ae301e792223873
Ceda EI <ceda_ei@webionite.com>
2019-12-29 21:32:03 +0530

    Update support for right prompt in themes.

--- themes/gradient.sh
+++ themes/gradient.sh
@@ -5,7 +5,19 @@ local gradient=(226 118 37 66 60 237 233)
 local fg_gradient=(16 16 16 16 16 15 15)
 local j=0
 for i in ${WISH_PLUGINS[@]}; do
-	[[ $i == "newline" ]] && j=0 && continue
+	[[ $i =~ newline$ ]] && j=0 && continue
+	eval WISH_${i^^}_BG=${gradient[$j]}
+	eval WISH_${i^^}_FG=${fg_gradient[$j]}
+	((j++))
+	if [[ $j -eq ${#gradient[@]} ]]; then
+		j=0
+	fi
+done
+
+j=0
+for ((idx=$((${#WISH_RIGHT_PLUGINS[@]} - 1)); idx >= 0; idx--)); do
+	i=${WISH_RIGHT_PLUGINS[$idx]}
+	[[ $i =~ newline$ ]] && j=0 && continue
 	eval WISH_${i^^}_BG=${gradient[$j]}
 	eval WISH_${i^^}_FG=${fg_gradient[$j]}
 	((j++))
--- themes/plain.sh
+++ themes/plain.sh
@@ -1,7 +1,7 @@
 WISH_DEFAULT_BG=-1
 WISH_DEFAULT_FG=-1
 WISH_POWERLINE=0
-for i in ${WISH_PLUGINS[@]}; do
+for i in ${WISH_PLUGINS[@]} ${WISH_RIGHT_PLUGINS[@]}; do
 	eval WISH_${i^^}_BG=-1
 	eval WISH_${i^^}_FG=-1
 done
