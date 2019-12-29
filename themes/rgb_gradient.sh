b1fe80cef305550c6fc2186a77562fe4ae6b9ff4
Ceda EI <ceda_ei@webionite.com>
2019-12-29 21:27:56 +0530

    Updated rgb_gradient for better assigning in right prompt

--- themes/rgb_gradient.sh
+++ themes/rgb_gradient.sh
@@ -5,7 +5,7 @@ local gradient=(ffff5f 7ad767 66b097 5e7388 534d61 3a3338 121212)
 local fg_gradient=(000000 000000 000000 000000 000000 ffffff ffffff)
 local j=0
 for i in ${WISH_PLUGINS[@]}; do
-	[[ $i == "newline" ]] && j=0 && continue
+	[[ $i =~ newline$ ]] && j=0 && continue
 	eval WISH_${i^^}_BG=${gradient[$j]}
 	eval WISH_${i^^}_FG=${fg_gradient[$j]}
 	((j++))
@@ -15,13 +15,11 @@ for i in ${WISH_PLUGINS[@]}; do
 done
 
 j=0
-for i in ${WISH_RIGHT_PLUGINS[@]}; do
-	[[ $i == "newline" ]] && j=0 && continue
-	eval WISH_${i^^}_BG=${gradient[$((${#WISH_RIGHT_PLUGINS[@]} - $j - 1))]}
-	x=WISH_${i^^}_BG
-	echo WISH_${i^^}_BG: ${!x} ${#gradient[@]} - ${#WISH_RIGHT_PLUGINS[@]} - $j
-	eval WISH_${i^^}_FG=${fg_gradient[$((${#WISH_RIGHT_PLUGINS[@]} - $j - 1))]}
-	echo WISH_${i^^}_FG: $WISH_${i^^}_FG
+for ((idx=$((${#WISH_RIGHT_PLUGINS[@]} - 1)); idx >= 0; idx--)); do
+	i=${WISH_RIGHT_PLUGINS[$idx]}
+	[[ $i =~ newline$ ]] && j=0 && continue
+	eval WISH_${i^^}_BG=${gradient[$j]}
+	eval WISH_${i^^}_FG=${fg_gradient[$j]}
 	((j++))
 	if [[ $j -eq ${#gradient[@]} ]]; then
 		j=0
