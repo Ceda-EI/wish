9e55279faf8fa7406ee2f04a0818d40abb4b3ad9
Ceda EI <ceda_ei@webionite.com>
2019-12-31 13:33:55 +0530

    [Fix] Change wrong variable name in custom_text plugin

--- plugins/custom_text.sh
+++ plugins/custom_text.sh
@@ -10,7 +10,7 @@ function wish_custom_text_set_colors() {
 	WISH_CUSTOM_TEXT_FG=${WISH_CUSTOM_TEXT_FG:-$WISH_DEFAULT_FG}
 	WISH_CUSTOM_TEXT_BG=${WISH_CUSTOM_TEXT_BG:-$WISH_DEFAULT_BG}
 	local default_text='To set custom text here, add text="your text" in your config'
-	WISH_CUSTOM_TEXT_TEXT=${WISH_CUSTOM_TEXT:-$default_text}
+	WISH_CUSTOM_TEXT_TEXT=${WISH_CUSTOM_TEXT_TEXT:-$default_text}
 }
 
 function wish_custom_text_main() {
