4b9c2b6982be58c1844d81b1df826ee2c12668cb
Ceda EI <ceda_ei@webionite.com>
2020-05-25 17:17:53 +0530

    Add theme luminus

---
+++ themes/luminus.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function wish_luminous_theme {
+	local i
+	local gradient=(ef7126 f7894c fea16f ffb892 ffd0b5 ffe7da ffffff)
+	local fg_gradient=(000000 000000 000000 000000 000000 000000 000000)
+	local j=0
+	for i in "${WISH_PLUGINS[@]}"; do
+		[[ $i =~ newline$ ]] && j=0 && continue
+		eval "WISH_${i^^}_FG=${fg_gradient[$j]}"
+		eval "WISH_${i^^}_BG=${gradient[$j]}"
+		((j++))
+		if [[ $j -eq ${#gradient[@]} ]]; then
+			j=0
+		fi
+	done
+
+	j=0
+	for ((idx=$((${#WISH_RIGHT_PLUGINS[@]} - 1)); idx >= 0; idx--)); do
+		i=${WISH_RIGHT_PLUGINS[$idx]}
+		[[ $i =~ newline$ ]] && j=0 && continue
+		eval "WISH_${i^^}_BG=${gradient[$j]}"
+		eval "WISH_${i^^}_FG=${fg_gradient[$j]}"
+		((j++))
+		if [[ $j -eq ${#gradient[@]} ]]; then
+			j=0
+		fi
+	done
+}
+
+wish_luminous_theme
