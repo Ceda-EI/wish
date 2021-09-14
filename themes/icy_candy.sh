869a754ede67b10935164e2646a350ee6a66a66e
Ceda EI <ceda_ei@webionite.com>
2021-09-14 16:38:37 +0530

    Add theme icy_candy

---
+++ themes/icy_candy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function wish_icy_candy_theme {
+	local i
+	local l_gradient=(f5a9b8 f6b7c3 f8c5cf fad4db fbe2e7 fdf0f3 ffffff)
+	local r_gradient=(5bcefa 76d6fa 91defb ade6fc c8eefd e3f6fe ffffff)
+	local fg_gradient=(000000 000000 000000 000000 000000 000000 000000)
+	local j=0
+	for i in "${WISH_PLUGINS[@]}"; do
+		[[ $i =~ newline$ ]] && j=0 && continue
+		eval "WISH_${i^^}_FG=${fg_gradient[$j]}"
+		eval "WISH_${i^^}_BG=${l_gradient[$j]}"
+		((j++))
+		if [[ $j -eq ${#l_gradient[@]} ]]; then
+			j=0
+		fi
+	done
+
+	j=0
+	for ((idx=$((${#WISH_RIGHT_PLUGINS[@]} - 1)); idx >= 0; idx--)); do
+		i=${WISH_RIGHT_PLUGINS[$idx]}
+		[[ $i =~ newline$ ]] && j=0 && continue
+		eval "WISH_${i^^}_BG=${r_gradient[$j]}"
+		eval "WISH_${i^^}_FG=${fg_gradient[$j]}"
+		((j++))
+		if [[ $j -eq ${#r_gradient[@]} ]]; then
+			j=0
+		fi
+	done
+}
+
+wish_icy_candy_theme
